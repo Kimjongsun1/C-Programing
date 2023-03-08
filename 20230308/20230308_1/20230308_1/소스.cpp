155cbdb178649e6f9613c00d4e2cdbe093836a3c
Kimjongsun1 <125526341+Kimjongsun1@users.noreply.github.com>
2023-03-08 10:13:34 +0900

    Add files via upload

--- 20230308/20230308_1/20230308_1/소스.cpp
+++ 20230308/20230308_1/20230308_1/소스.cpp
@@ -4,7 +4,10 @@
 int main(void)
 {
 
-	printf("test \n");
+	printf("test \n"); //\n line feed 
+	printf("202315004\n"); //\n lime feed
+	printf("JongSun KIM\n"); //\n line feed
+                                   
 
 
 	return 0;
