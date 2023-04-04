25d786b9ed9c71b786eecd268508e3be4eec0ab8
Kimjongsun1 <125526341+Kimjongsun1@users.noreply.github.com>
2023-04-04 20:37:08 +0900

    Add files via upload

--- Physics_Trainer_Chap2/Physics_Trainer_Chap2.cpp
+++ Physics_Trainer_Chap2/Physics_Trainer_Chap2.cpp
@@ -18,7 +18,7 @@ void Excersize_2_2(int solution, int answer)
 
 	printf("\n\n");
 	printf("2-2 \n");
-	printf("황영조 선수는 %4.1lf m를 %d시간 %d분 %d초에 뛰어 우승하였다.\n\n", distance1 , h, m, s);
+	printf("황영조 선수는 %4.1lf m를 %d시간 %d분 %d초에 뛰어 우승하였다.\n\n", distance1, h, m, s);
 	printf("황영조 선수는 %4.1lf m를 평균 몇 초에 달린 셈인가?  \n", distance2);
 }
 
@@ -35,8 +35,8 @@ void Excersize_2_12(int solution, int answer)
 	printf("\n\n");
 	printf("2-12 \n");
 	printf("어떤 자동차가 %d s 동안에 %d m/s 에서 %d m/s로 가속된다.\n\n", sec, V, V0);
-	printf("이 자동차의 가속도는 얼마인가?  \n", acceleration);
-	printf("이 자동차가 그동안 달린 거리는 얼마인가?  \n", distance);
+	printf("(a) 이 자동차의 가속도는 얼마인가?  \n\n", acceleration);
+	printf("(b) 이 자동차가 그동안 달린 거리는 얼마인가?  \n", distance);
 }
 
 void Excersize_2_22(int solution, int answer)
@@ -48,8 +48,8 @@ void Excersize_2_22(int solution, int answer)
 	printf("\n\n");
 	printf("2-22 \n");
 	printf("어떤 높이에서 %d m/s의 속도로 연직 아래로 공을 던졌다.\n\n", V);
-	printf("(a) 공을 던진 후 %d s 후의 속도를 구하라.\n", sec);
-	printf("(b) 그동안 낙하한 거리는 얼마인가?\n", distance);	
+	printf("(a) 공을 던진 후 %d s 후의 속도를 구하라.\n\n", sec);
+	printf("(b) 그동안 낙하한 거리는 얼마인가?\n", distance);
 }
 
 
@@ -66,7 +66,7 @@ int main(void)
 	printf("=================================================================\n");
 
 
-	Excersize_2_2(0,0);
-	Excersize_2_12(0,0);
-	Excersize_2_22(0,0);
+	Excersize_2_2(0, 0);
+	Excersize_2_12(0, 0);
+	Excersize_2_22(0, 0);
 }
