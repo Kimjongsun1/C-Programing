ccba37dc2e3c4c3afc2944d4d938d08737200ad9
Kimjongsun1 <125526341+Kimjongsun1@users.noreply.github.com>
2023-04-23 22:08:41 +0900

    Add files via upload

---
+++ 20230423-3/main.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int n;
+	int sum;
+
+	printf("정수를 입력하시오\n");
+	scanf("%d", &n);
+
+	i = 1;
+	sum = 0;
+
+	while (i <= n)
+	{
+		sum += i;
+		i++;
+	}
+	printf("1부터 %d까지의 합은 %d입니다\n", n, sum);
+	return 0;
+}
